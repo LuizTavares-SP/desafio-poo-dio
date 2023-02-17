f1892d6cf543c6f41efe4d524989ab3c056526fa
Mr-Robot-PPADS <118316951+Lais-e-Luiz@users.noreply.github.com>
2023-02-16 22:58:55 -0300

    Projeto final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -27,9 +29,33 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp java = new Bootcamp();
+        java.setNome("Java Developer");
+        java.setDescricao("Bootcamp Java Developer");
+        java.getConteudos().add(curso);
+        java.getConteudos().add(curso2);
+        java.getConteudos().add(mentoria);
 
+        Dev developerLuiz = new Dev();
+        developerLuiz.setNome("Luiz");
+        developerLuiz.inscreverBootcamp(java);
+
+        System.out.println("Conteúdos inscritos: " + developerLuiz.getConteudosInscritos());
+        developerLuiz.progredir();
+        developerLuiz.progredir();
+        System.out.println("Conteúdos inscritos: " + developerLuiz.getConteudosInscritos());
+        System.out.println("Conteúdos concluidos: " + developerLuiz.getConteudoConcluidos());
+        System.out.println("XP: " + developerLuiz.calcularTotalXp());
+
+
+        Dev developerEliana = new Dev();
+        developerEliana.setNome("Eliana");
+        developerEliana.inscreverBootcamp(java);
+
+        System.out.printf("Conteúdos inscritos: %s%n", developerEliana.getConteudosInscritos());
+        developerEliana.progredir();
+        System.out.printf("Conteúdos inscritos: %s%n", developerEliana.getConteudosInscritos());
+        System.out.printf("Conteúdos concluidos: %s%n", developerLuiz.getConteudoConcluidos());
+        System.out.println("XP: " + developerEliana.calcularTotalXp());
     }
 }
